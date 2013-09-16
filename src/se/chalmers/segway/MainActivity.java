0ebd5cb3d8b5a281c9fc5aee158356c0c99e265c
Glazastik <anton.myrholm@gmail.com>
2013-09-16 16:13:40 +0200

    Made our MainActivity ready for AndEngine

--- src/se/chalmers/segway/MainActivity.java
+++ src/se/chalmers/segway/MainActivity.java
@@ -1,41 +1,58 @@
 package se.chalmers.segway;
 
+import org.andengine.engine.Engine;
+import org.andengine.engine.LimitedFPSEngine;
+import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.WakeLockOptions;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.ui.activity.BaseGameActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends BaseGameActivity
-{
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class MainActivity extends BaseGameActivity {
+	
+	private Camera camera;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 
 	@Override
 	public EngineOptions onCreateEngineOptions() {
-		return null;
+		camera = new Camera(0, 0, 800, 480);
+	    EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(800, 480), this.camera);
+	    engineOptions.getAudioOptions().setNeedsMusic(true).setNeedsSound(true);
+	    engineOptions.setWakeLockOptions(WakeLockOptions.SCREEN_ON);
+	    return engineOptions;
+	}
+
+	@Override
+	public Engine onCreateEngine(EngineOptions pEngineOptions) {
+		return new LimitedFPSEngine(pEngineOptions, 60);
+		
 	}
 
 	@Override
 	public void onCreateResources(
 			OnCreateResourcesCallback pOnCreateResourcesCallback)
 			throws Exception {
-		
+
 	}
 
 	@Override
 	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
 			throws Exception {
-		
+
 	}
 
 	@Override
 	public void onPopulateScene(Scene pScene,
 			OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
-		
+
 	}
 }
