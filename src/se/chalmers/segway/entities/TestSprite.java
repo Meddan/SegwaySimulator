c5ce131b71acde4e2e09da1c33e0c9a213d1f0a7
Victor Nilsson <trivoc92@gmail.com>
2013-09-18 12:49:55 +0200

    Added functionality to TestSprite

--- src/se/chalmers/segway/entities/TestSprite.java
+++ src/se/chalmers/segway/entities/TestSprite.java
@@ -1,14 +1,19 @@
 package se.chalmers.segway.entities;
 
 import org.andengine.engine.camera.Camera;
+import org.andengine.engine.handler.physics.PhysicsHandler;
 import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.texture.region.TextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 import se.chalmers.segway.managers.ResourcesManager;
 
@@ -19,12 +24,11 @@ import se.chalmers.segway.managers.ResourcesManager;
  */
 public class TestSprite extends AnimatedSprite {
    
-    
     private Camera characterCam;
     private PhysicsWorld physWorld;
     private Body body;
     
-    private boolean canJump;
+    private boolean canRun;
     
   
     //Skicka in VBOMAnager från en metod i MainActivity(?) (mEngine.getVertexBuffer...)
@@ -32,11 +36,56 @@ public class TestSprite extends AnimatedSprite {
 	   Camera camera, PhysicsWorld physWorld){
        super(xPos, yPos, ResourcesManager.getInstance().playerRegion, vboManager);
        
-       this.canJump = false;
+       this.characterCam = camera;
+       this.canRun = false;
        this.physWorld = physWorld;
+       
+       createPhysics(camera, physWorld);
+       characterCam.setChaseEntity(this);
        
    }
    
    
+   private void createPhysics(final Camera camera, PhysicsWorld physicsWorld)
+   {        
+       body = PhysicsFactory.createBoxBody(physicsWorld, this, BodyType.DynamicBody, PhysicsFactory.createFixtureDef(0, 0, 0));
+
+       body.setUserData("player");
+       body.setFixedRotation(true);
+       
+       physicsWorld.registerPhysicsConnector(new PhysicsConnector(this, body, true, false)
+       {
+           public void onUpdate(float pSecondsElapsed)
+           {
+               super.onUpdate(pSecondsElapsed);
+               camera.onUpdate(0.1f);
+               
+               if (getY() <= 0)
+               {                    
+                   onDie();
+               }
+               
+               if (canRun)
+               {    
+                   body.setLinearVelocity(new Vector2(5, body.getLinearVelocity().y)); 
+               }
+           }
+       });
+   }
+   
+   public void onDie(){
+       
+   }
+   
+   public void setRunning()
+   {
+       canRun = true;       
+       final long[] PLAYER_ANIMATE = new long[] { 100, 100, 100 };
+       animate(PLAYER_ANIMATE, 0, 2, true);
+   }
+   
+   public void jump(){
+       body.setLinearVelocity(new Vector2(body.getLinearVelocity().x, 150));
+   }
    
 }
