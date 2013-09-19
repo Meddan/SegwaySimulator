3100a975dba7f5fa239414f3742e63afb1cc477f
Wiixtor <wiixtor@gmail.com>
2013-09-19 11:15:31 +0200

    Added Player class and implemeted it

--- src/se/chalmers/segway/scenes/GameScene.java
+++ src/se/chalmers/segway/scenes/GameScene.java
@@ -6,6 +6,8 @@ import org.andengine.engine.camera.hud.HUD;
 import org.andengine.entity.IEntity;
 import org.andengine.entity.modifier.LoopEntityModifier;
 import org.andengine.entity.modifier.ScaleModifier;
+import org.andengine.entity.scene.IOnSceneTouchListener;
+import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.text.Text;
@@ -13,6 +15,7 @@ import org.andengine.entity.text.TextOptions;
 import org.andengine.extension.physics.box2d.FixedStepPhysicsWorld;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.util.SAXUtils;
 import org.andengine.util.adt.align.HorizontalAlign;
 import org.andengine.util.adt.color.Color;
@@ -26,26 +29,30 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 
+import se.chalmers.segway.entities.Player;
 import se.chalmers.segway.managers.SceneManager;
 import se.chalmers.segway.managers.SceneManager.SceneType;
 
-public class GameScene extends BaseScene {
+public class GameScene extends BaseScene implements IOnSceneTouchListener {
 
-		/**
+	/**
 	 * Variables
 	 */
 	private HUD gameHUD;
 	private Text scoreText;
 	private int score;
 	private PhysicsWorld physicsWorld;
-	
+
+	private Player player;
+
 	private static final String TAG_ENTITY = "entity";
 	private static final String TAG_ENTITY_ATTRIBUTE_X = "x";
 	private static final String TAG_ENTITY_ATTRIBUTE_Y = "y";
 	private static final String TAG_ENTITY_ATTRIBUTE_TYPE = "type";
-	    
+
 	private static final Object TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM1 = "platform1";
 	private static final Object TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_COIN = "coin";
+	private static final Object TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLAYER = "player";
 
 	/**
 	 * Methods
@@ -56,6 +63,7 @@ public class GameScene extends BaseScene {
 		createHUD();
 		createPhysics();
 		loadLevel(1);
+		setOnSceneTouchListener(this);
 	}
 
 	@Override
@@ -104,73 +112,117 @@ public class GameScene extends BaseScene {
 		physicsWorld = new FixedStepPhysicsWorld(60, new Vector2(0, -17), false);
 		registerUpdateHandler(physicsWorld);
 	}
-	
-	private void loadLevel(int levelID)
-	{
-	    final SimpleLevelLoader levelLoader = new SimpleLevelLoader(vbom);
-	    
-	    final FixtureDef FIXTURE_DEF = PhysicsFactory.createFixtureDef(0, 0.01f, 0.5f);
-	    
-	    levelLoader.registerEntityLoader(new EntityLoader<SimpleLevelEntityLoaderData>(LevelConstants.TAG_LEVEL)
-	    {
-	        public IEntity onLoadEntity(final String pEntityName, final IEntity pParent, final Attributes pAttributes, final SimpleLevelEntityLoaderData pSimpleLevelEntityLoaderData) throws IOException 
-	        {
-	            final int width = SAXUtils.getIntAttributeOrThrow(pAttributes, LevelConstants.TAG_LEVEL_ATTRIBUTE_WIDTH);
-	            final int height = SAXUtils.getIntAttributeOrThrow(pAttributes, LevelConstants.TAG_LEVEL_ATTRIBUTE_HEIGHT);
-	            
-	            // TODO later we will specify camera BOUNDS and create invisible walls
-	            // on the beginning and on the end of the level.
 
-	            return GameScene.this;
-	        }
-	    });
-	    
-	    levelLoader.registerEntityLoader(new EntityLoader<SimpleLevelEntityLoaderData>(TAG_ENTITY)
-	    {
-	        public IEntity onLoadEntity(final String pEntityName, final IEntity pParent, final Attributes pAttributes, final SimpleLevelEntityLoaderData pSimpleLevelEntityLoaderData) throws IOException
-	        {
-	            final int x = SAXUtils.getIntAttributeOrThrow(pAttributes, TAG_ENTITY_ATTRIBUTE_X);
-	            final int y = SAXUtils.getIntAttributeOrThrow(pAttributes, TAG_ENTITY_ATTRIBUTE_Y);
-	            final String type = SAXUtils.getAttributeOrThrow(pAttributes, TAG_ENTITY_ATTRIBUTE_TYPE);
-	            
-	            final Sprite levelObject;
-	            
-	            if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM1))
-	            {
-	                levelObject = new Sprite(x, y, resourcesManager.platform1_region, vbom);
-	                PhysicsFactory.createBoxBody(physicsWorld, levelObject, BodyType.StaticBody, FIXTURE_DEF).setUserData("platform1");
-	            } 
-	            else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_COIN))
-	            {
-	                levelObject = new Sprite(x, y, resourcesManager.coin_region, vbom)
-	                {
-	                    @Override
-	                    protected void onManagedUpdate(float pSecondsElapsed) 
-	                    {
-	                        super.onManagedUpdate(pSecondsElapsed);
-	                        
-	                        /** 
-	                         * TODO
-	                         * we will later check if player collide with this (coin)
-	                         * and if it does, we will increase score and hide coin
-	                         * it will be completed in next articles (after creating player code)
-	                         */
-	                    }
-	                };
-	                levelObject.registerEntityModifier(new LoopEntityModifier(new ScaleModifier(1, 1, 1.3f)));
-	            }            
-	            else
-	            {
-	                throw new IllegalArgumentException();
-	            }
+	//Handles all code for loading levels
+	private void loadLevel(int levelID) {
+		final SimpleLevelLoader levelLoader = new SimpleLevelLoader(vbom);
 
-	            levelObject.setCullingEnabled(true);
+		final FixtureDef FIXTURE_DEF = PhysicsFactory.createFixtureDef(0,
+				0.01f, 0.5f);
 
-	            return levelObject;
-	        }
-	    });
+		levelLoader
+				.registerEntityLoader(new EntityLoader<SimpleLevelEntityLoaderData>(
+						LevelConstants.TAG_LEVEL) {
+					public IEntity onLoadEntity(
+							final String pEntityName,
+							final IEntity pParent,
+							final Attributes pAttributes,
+							final SimpleLevelEntityLoaderData pSimpleLevelEntityLoaderData)
+							throws IOException {
+						final int width = SAXUtils.getIntAttributeOrThrow(
+								pAttributes,
+								LevelConstants.TAG_LEVEL_ATTRIBUTE_WIDTH);
+						final int height = SAXUtils.getIntAttributeOrThrow(
+								pAttributes,
+								LevelConstants.TAG_LEVEL_ATTRIBUTE_HEIGHT);
 
-	    levelLoader.loadLevelFromAsset(activity.getAssets(), "level/" + levelID + ".lvl");
+						// TODO later we will specify camera BOUNDS and create
+						// invisible walls
+						// on the beginning and on the end of the level.
+
+						return GameScene.this;
+					}
+				});
+
+		levelLoader
+				.registerEntityLoader(new EntityLoader<SimpleLevelEntityLoaderData>(
+						TAG_ENTITY) {
+					public IEntity onLoadEntity(
+							final String pEntityName,
+							final IEntity pParent,
+							final Attributes pAttributes,
+							final SimpleLevelEntityLoaderData pSimpleLevelEntityLoaderData)
+							throws IOException {
+						final int x = SAXUtils.getIntAttributeOrThrow(
+								pAttributes, TAG_ENTITY_ATTRIBUTE_X);
+						final int y = SAXUtils.getIntAttributeOrThrow(
+								pAttributes, TAG_ENTITY_ATTRIBUTE_Y);
+						final String type = SAXUtils.getAttributeOrThrow(
+								pAttributes, TAG_ENTITY_ATTRIBUTE_TYPE);
+
+						final Sprite levelObject;
+
+						//Cases for loading different objects
+						//Loads platform1
+						if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM1)) {
+							levelObject = new Sprite(x, y,
+									resourcesManager.platform1_region, vbom);
+							PhysicsFactory.createBoxBody(physicsWorld,
+									levelObject, BodyType.StaticBody,
+									FIXTURE_DEF).setUserData("platform1");
+						//Loads coin
+						} else if (type
+								.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_COIN)) {
+							levelObject = new Sprite(x, y,
+									resourcesManager.coin_region, vbom) {
+								@Override
+								protected void onManagedUpdate(
+										float pSecondsElapsed) {
+									super.onManagedUpdate(pSecondsElapsed);
+
+									/**
+									 * TODO we will later check if player
+									 * collide with this (coin) and if it does,
+									 * we will increase score and hide coin it
+									 * will be completed in next articles (after
+									 * creating player code)
+									 */
+								}
+							};
+							levelObject
+									.registerEntityModifier(new LoopEntityModifier(
+											new ScaleModifier(1, 1, 1.3f)));
+						//Loading player type objects
+						} else if (type
+								.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLAYER)) {
+							player = new Player(x, y, vbom, camera,
+									physicsWorld) {
+								@Override
+								public void onDie() {
+									// TODO Latter we will handle it.
+								}
+							};
+							levelObject = player;
+						} else {
+							throw new IllegalArgumentException();
+						}
+
+						levelObject.setCullingEnabled(true);
+
+						return levelObject;
+					}
+				});
+
+		levelLoader.loadLevelFromAsset(activity.getAssets(), "level/" + levelID
+				+ ".lvl");
+	}
+
+	@Override
+	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+		if (pSceneTouchEvent.isActionDown()) {
+
+		}
+		return false;
 	}
 
 }
