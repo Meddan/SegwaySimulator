f43f6f3bae1f78005a45cbcb6a7a36714082172b
Wiixtor <wiixtor@gmail.com>
2013-09-23 13:09:37 +0200

    Added some animations

--- src/se/chalmers/segway/entities/Player.java
+++ src/se/chalmers/segway/entities/Player.java
@@ -14,40 +14,46 @@ import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import se.chalmers.segway.managers.ResourcesManager;
 
 public abstract class Player extends AnimatedSprite {
-	
+
 	private Body body;
 	private int footContacts = 0;
-	
-    public Player(float pX, float pY, VertexBufferObjectManager vbo, Camera camera, PhysicsWorld physicsWorld)
-    {
-        super(pX, pY, ResourcesManager.getInstance().player_region, vbo);
-        createPhysics(camera, physicsWorld);
-        camera.setChaseEntity(this);
-    }
-    
-    private void createPhysics(final Camera camera, PhysicsWorld physicsWorld)
-    {        
-        body = PhysicsFactory.createBoxBody(physicsWorld, this, BodyType.DynamicBody, PhysicsFactory.createFixtureDef(0, 0, 0));
 
-        body.setUserData("player");
-        body.setFixedRotation(true);
-        
-        physicsWorld.registerPhysicsConnector(new PhysicsConnector(this, body, true, false)
-        {
-            @Override
-            public void onUpdate(float pSecondsElapsed)
-            {
-                super.onUpdate(pSecondsElapsed);
-                camera.onUpdate(0.1f);
-                
-                if (getY() <= 0)
-                {                    
-                    onDie();
-                }
-            }
-        });
-    }
-    
+	public Player(float pX, float pY, VertexBufferObjectManager vbo,
+			Camera camera, PhysicsWorld physicsWorld) {
+		super(pX, pY, ResourcesManager.getInstance().player_region, vbo);
+		createPhysics(camera, physicsWorld);
+		camera.setChaseEntity(this);
+	}
+
+	private void createPhysics(final Camera camera, PhysicsWorld physicsWorld) {
+		body = PhysicsFactory.createBoxBody(physicsWorld, this,
+				BodyType.DynamicBody, PhysicsFactory.createFixtureDef(0, 0, 0));
+
+		body.setUserData("player");
+		body.setFixedRotation(true);
+
+		physicsWorld.registerPhysicsConnector(new PhysicsConnector(this, body,
+				true, false) {
+			@Override
+			public void onUpdate(float pSecondsElapsed) {
+				super.onUpdate(pSecondsElapsed);
+				camera.onUpdate(0.1f);
+
+				if (getY() <= 0) {
+					onDie();
+				}
+
+				if (body.getLinearVelocity().x < 0) {
+					final long[] PLAYER_ANIMATE = new long[] { 50, 50, 50 };
+					animate(PLAYER_ANIMATE, 0, 2, true);
+				} //else if (body.getLinearVelocity().x > 0) {
+				//	final long[] PLAYER_ANIMATE = new long[] { 50, 50, 50 };
+				//	animate(PLAYER_ANIMATE, 0, 2, true);
+				//}
+			}
+		});
+	}
+
 	public void increaseFootContacts() {
 		footContacts++;
 	}
@@ -55,20 +61,18 @@ public abstract class Player extends AnimatedSprite {
 	public void decreaseFootContacts() {
 		footContacts--;
 	}
-	
+
 	public void setSpeed(Vector2 v) {
 		body.setLinearVelocity(v.x, body.getLinearVelocity().y);
 	}
-    
-    public void jump()
-    {
-        if (footContacts < 1) 
-        {
-            return; 
-        }	
-        body.setLinearVelocity(new Vector2(body.getLinearVelocity().x, 6)); 
-    }
-    
-    public abstract void onDie();
-    
+
+	public void jump() {
+		if (footContacts < 1) {
+			return;
+		}
+		body.setLinearVelocity(new Vector2(body.getLinearVelocity().x, 6));
+	}
+
+	public abstract void onDie();
+
 }
