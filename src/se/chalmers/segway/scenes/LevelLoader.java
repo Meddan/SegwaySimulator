ce5f3d0484bec2b75df24d9c2079d5693e2083f5
Pontus Doverstav <doverstav@hotmail.com>
2013-10-11 14:16:28 +0200

    Added import

--- src/se/chalmers/segway/scenes/LevelLoader.java
+++ src/se/chalmers/segway/scenes/LevelLoader.java
@@ -19,6 +19,7 @@ import org.xml.sax.Attributes;
 import se.chalmers.segway.entities.Player;
 import se.chalmers.segway.resources.ResourcesManager;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
