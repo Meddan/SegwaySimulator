4d69a9d478c54cc86c2e29584fb3b245c958b717
Christoffer Medin <christoffer.medin@gmail.com>
2013-10-14 15:51:14 +0200

    Added more documentation to SaveManager.

--- src/se/chalmers/segway/game/SaveManager.java
+++ src/se/chalmers/segway/game/SaveManager.java
@@ -19,6 +19,7 @@ public class SaveManager {
 	 * sessions and enables them.
 	 */
 	public static void loadUpgrades() {
+		//Creates a path to the folder where the upgrades are saved.
 		File path=new File(ResourcesManager.getInstance().activity.getFilesDir(),"saves");
 		File file = new File(path, "upgrades");
 		if(file.exists()){
@@ -27,6 +28,7 @@ public class SaveManager {
 				ObjectInputStream ois = new ObjectInputStream(fis);
 				Object obj = ois.readObject();
 				ois.close();
+				//Iterates over the hashmap of upgrades and matches them by name with the upgrades in the class Upgrades.
 				if (obj != null && obj instanceof HashMap<?, ?>){
 					for(Object i : ((HashMap<?,?>)obj).keySet()){
 						for(Upgrades u : Upgrades.values()){
@@ -46,10 +48,12 @@ public class SaveManager {
 	 * Writes which upgrades have been bought to a file.
 	 */
 	public static void saveUpgrades() {
+		//Saves the upgrades in a hashmap. Identifies via name and saves whether or not they are enabled.
 		HashMap<String,Boolean> saveMap = new HashMap<String, Boolean>();
 		for(Upgrades upg : Upgrades.values()){
 			saveMap.put(upg.getName(), upg.isActivated());
 		}
+		//The path where the file will be saved
 		File path=new File(ResourcesManager.getInstance().activity.getFilesDir(),"saves");
 		path.mkdir();
 		FileOutputStream fos;
@@ -61,7 +65,6 @@ public class SaveManager {
 			oos.close();
 		} catch (Exception e) {
 			e.printStackTrace();
-			
 		}
 	}
 	/**
@@ -69,6 +72,7 @@ public class SaveManager {
 	 * @param data the playerdata to be saved
 	 */
 	public static void savePlayerData(PlayerData data){
+		//The path to the file
 		File path=new File(ResourcesManager.getInstance().activity.getFilesDir(),"saves");
 		path.mkdir();
 		FileOutputStream fos;
@@ -84,14 +88,14 @@ public class SaveManager {
 	}
 	/**
 	 * Returns the players saved data
-	 * @return PlayerData from file, null if something went wrong, the file doesn't exist
+	 * @return PlayerData from file, null if the file doesn't exist (the player has never saved). 
 	 */
 	public static PlayerData loadPlayerData(){
+		//The path to the file
 		File path=new File(ResourcesManager.getInstance().activity.getFilesDir(),"saves");
 		File file = new File(path, "player");
 		if(file.exists()){
 			try {
-				//TODO: Needs testing
 				FileInputStream fis = new FileInputStream(file);
 				ObjectInputStream ois = new ObjectInputStream(fis);
 				Object obj = ois.readObject();
@@ -110,6 +114,10 @@ public class SaveManager {
 			return null;
 		}
 	}
+	/**
+	 * Saves settings to a file
+	 * @param settings the settings object to be saved
+	 */
 	public static void saveSettings(Settings settings){
 		File path=new File(ResourcesManager.getInstance().activity.getFilesDir(),"saves");
 		path.mkdir();
@@ -124,6 +132,10 @@ public class SaveManager {
 			e.printStackTrace();
 		}
 	}
+	/**
+	 * Loads settings from a file
+	 * @return the settings. Null if no file was found.
+	 */
 	public static Settings loadSettings(){
 		File path=new File(ResourcesManager.getInstance().activity.getFilesDir(),"saves");
 		File file = new File(path, "settings");
