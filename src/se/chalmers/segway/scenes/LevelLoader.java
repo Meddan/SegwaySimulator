fed0a9a62ca3d96a10d81f2a22147866f6a6bb59
Pontus Doverstav <doverstav@hotmail.com>
2013-10-18 15:53:39 +0200

    Added documentation for LevelLoader

--- src/se/chalmers/segway/scenes/LevelLoader.java
+++ src/se/chalmers/segway/scenes/LevelLoader.java
@@ -34,13 +34,28 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 
+/**
+ * This class can be used to load a level from an XML-file (assuming it is
+ * written correctly) into a provided {@link GameScene}.
+ * 
+ * @author Dover
+ * 
+ */
+
 public class LevelLoader extends EntityLoader<SimpleLevelEntityLoaderData> {
 
+	/*
+	 * Tags for critical information such as position on screen etc
+	 */
 	private static final String TAG_ENTITY = "entity";
 	private static final String TAG_ENTITY_ATTRIBUTE_X = "x";
 	private static final String TAG_ENTITY_ATTRIBUTE_Y = "y";
 	private static final String TAG_ENTITY_ATTRIBUTE_TYPE = "type";
 
+	/*
+	 * Tags necessary for identifying different kinds of entities while loading
+	 * the level
+	 */
 	private static final Object TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM1 = "platform1";
 	private static final Object TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM2 = "platform2";
 	private static final Object TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM3 = "platform3";
@@ -69,6 +84,22 @@ public class LevelLoader extends EntityLoader<SimpleLevelEntityLoaderData> {
 	private FixtureDef FIXTURE_DEF;
 	private FixtureDef zoneFixtureDef;
 
+	/**
+	 * Creates a new LevelLoader with the provided {@link PhysicsWorld},
+	 * {@link Player} and {@link GameScene}.
+	 * 
+	 * It is important that the PhysicsWorld and Player sent to this class are
+	 * the EXACT same as the ones in the provided GameScene, otherwise the code
+	 * will not function properly and stuff will break. And broken code kills
+	 * kittens.
+	 * 
+	 * @param pw
+	 *            the PhysicsWorld that is used by the provided GameScene
+	 * @param p
+	 *            the Player that is used by the provided GameScene
+	 * @param gs
+	 *            the GameScene that is loading a level using this class
+	 */
 	public LevelLoader(PhysicsWorld pw, Player p, GameScene gs) {
 		super(TAG_ENTITY);
 		this.init();
@@ -90,6 +121,9 @@ public class LevelLoader extends EntityLoader<SimpleLevelEntityLoaderData> {
 		zoneFixtureDef.isSensor = true;
 	}
 
+	/**
+	 * Loads all entities into the level.
+	 */
 	public IEntity onLoadEntity(final String pEntityName,
 			final IEntity pParent, final Attributes pAttributes,
 			final SimpleLevelEntityLoaderData pSimpleLevelEntityLoaderData)
@@ -104,25 +138,37 @@ public class LevelLoader extends EntityLoader<SimpleLevelEntityLoaderData> {
 		final Sprite levelObject;
 
 		// Cases for loading different objects
-		// Loads platform1
-		if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM1)) {
+		if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM1)) { // Loads
+																		// platform1
 			levelObject = loadPlatform(x, y, "platform1",
 					resourcesManager.platform1_region);
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM2)) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM2)) { // Loads
+																				// platform2
 			levelObject = loadPlatform(x, y, "platform2",
 					resourcesManager.platform2_region);
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM3)) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM3)) { // Loads
+																				// platform3
 			levelObject = loadPlatform(x, y, "platform3",
 					resourcesManager.platform3_region);
 		} else if (type
-				.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_STANDINGPLATFORM)) {
+				.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_STANDINGPLATFORM)) { // Loads
+																				// a
+																				// standing
+																				// platform
 			levelObject = loadPlatform(x, y, "StandingPlatform",
 					resourcesManager.StandingPlatform_region);
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM_CUBE)) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM_CUBE)) { // Loads
+																					// a
+																					// cubed
+																					// platfor
 			levelObject = loadPlatform(x, y, "Platform_Cube",
 					resourcesManager.platform_cube_region);
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_SPIKES)) {
-			levelObject = new Sprite(x, y, resourcesManager.spikes_region, vbom) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_SPIKES)) { // Loads
+																			// lethal
+																			// spikes
+			levelObject = new Sprite(x, y, resourcesManager.spikes_region, vbom) { // Creates
+																					// the
+																					// collisionlistener
 				@Override
 				protected void onManagedUpdate(float pSecondsElapsed) {
 					super.onManagedUpdate(pSecondsElapsed);
@@ -139,19 +185,27 @@ public class LevelLoader extends EntityLoader<SimpleLevelEntityLoaderData> {
 			physicsWorld.registerPhysicsConnector(new PhysicsConnector(
 					levelObject, body, true, false));
 
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_BOULDER)) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_BOULDER)) { // Loads
+																			// deadly
+																			// boulder
 
 			levelObject = new Boulder(x, y, physicsWorld, FIXTURE_DEF, player);
 
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_FALLING_SPIKE)) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_FALLING_SPIKE)) { // Loads
+																					// falling
+																					// spikes
 
 			levelObject = new FallSpike(x, y, physicsWorld, FIXTURE_DEF, player);
 
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_SPRING)) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_SPRING)) { // Loads
+																			// a
+																			// spring
 			levelObject = new AnimatedSprite(x, y,
 					resourcesManager.spring_region, vbom) {
 				@Override
-				protected void onManagedUpdate(float pSecondsElapsed) {
+				protected void onManagedUpdate(float pSecondsElapsed) { // Creates
+																		// the
+																		// collisionlistener
 					super.onManagedUpdate(pSecondsElapsed);
 
 					if (player.collidesWith(this) && player.getY() - 40 > y) {
@@ -167,12 +221,16 @@ public class LevelLoader extends EntityLoader<SimpleLevelEntityLoaderData> {
 			physicsWorld.registerPhysicsConnector(new PhysicsConnector(
 					levelObject, body, true, false));
 
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_COIN)) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_COIN)) { // Loads
+																		// a
+																		// coin
 			ITextureRegion cookie = resourcesManager.cookies_region
 					.getTextureRegion((int) (Math.random() * 8));
 			levelObject = new Sprite(x, y, cookie, vbom) {
 				@Override
-				protected void onManagedUpdate(float pSecondsElapsed) {
+				protected void onManagedUpdate(float pSecondsElapsed) { // Creates
+																		// the
+																		// collisionlistener
 					super.onManagedUpdate(pSecondsElapsed);
 
 					if (player.collidesWith(this)) {
@@ -186,12 +244,24 @@ public class LevelLoader extends EntityLoader<SimpleLevelEntityLoaderData> {
 			levelObject.registerEntityModifier(new LoopEntityModifier(
 					new ScaleModifier(1, 0.4f, 0.6f)));
 			// Loading player type objects
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLAYER)) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLAYER)) { // Loads
+																			// the
+																			// player
 			player.setRealPosition(x, y);
 
 			levelObject = player;
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_GOLDEN_COOKIE)) {
-			levelObject = new Sprite(x, y, resourcesManager.golden_cookie, vbom) {
+			// Doesn't quite load the player. While he is inserted into the
+			// scene,
+			// the object has already been created and the only thing that
+			// changes
+			// is the position of it.
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_GOLDEN_COOKIE)) { // Loads
+																					// the
+																					// golden
+																					// cookie
+			levelObject = new Sprite(x, y, resourcesManager.golden_cookie, vbom) { // Creates
+																					// the
+																					// collisionlistener
 				@Override
 				protected void onManagedUpdate(float pSecondsElapsed) {
 					super.onManagedUpdate(pSecondsElapsed);
@@ -205,8 +275,12 @@ public class LevelLoader extends EntityLoader<SimpleLevelEntityLoaderData> {
 				}
 			};
 
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_GASTANK)) {
-			levelObject = new Sprite(x, y, resourcesManager.gastank, vbom) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_GASTANK)) { // Loads
+																			// a
+																			// gastank
+			levelObject = new Sprite(x, y, resourcesManager.gastank, vbom) { // Creates
+																				// the
+																				// collisionlistener
 				@Override
 				protected void onManagedUpdate(float pSecondsElapsed) {
 					super.onManagedUpdate(pSecondsElapsed);
@@ -218,15 +292,27 @@ public class LevelLoader extends EntityLoader<SimpleLevelEntityLoaderData> {
 					}
 				}
 			};
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_ZONE_DOWN)) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_ZONE_DOWN)) { // Loads
+																				// a
+																				// zone
+																				// down
 			levelObject = loadZone(x, y, "zone_down",
 					resourcesManager.zone_down);
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_ZONE_UP)) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_ZONE_UP)) { // Loads
+																			// a
+																			// zone
+																			// up
 			levelObject = loadZone(x, y, "zone_up", resourcesManager.zone_up);
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_ZONE_LEFT)) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_ZONE_LEFT)) { // Loads
+																				// a
+																				// zone
+																				// left
 			levelObject = loadZone(x, y, "zone_left",
 					resourcesManager.zone_left);
-		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_ZONE_RIGHT)) {
+		} else if (type.equals(TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_ZONE_RIGHT)) { // Loads
+																				// a
+																				// zone
+																				// right
 			levelObject = loadZone(x, y, "zone_right",
 					resourcesManager.zone_right);
 		} else {
@@ -238,6 +324,20 @@ public class LevelLoader extends EntityLoader<SimpleLevelEntityLoaderData> {
 		return levelObject;
 	}
 
+	/**
+	 * Generalized method for loading zones.
+	 * 
+	 * @param x
+	 *            x-position of the zone
+	 * @param y
+	 *            y-posiiton of the zone
+	 * @param zone
+	 *            the string which will be used to identify the zone
+	 * @param zoneRegion
+	 *            the texture that will be used to show the zone
+	 * 
+	 * @return a zone created with the specified parameters
+	 */
 	private Sprite loadZone(int x, int y, String zone, ITextureRegion zoneRegion) {
 		Sprite zoneSprite = new Sprite(x, y, zoneRegion, vbom);
 		final Body body = PhysicsFactory.createBoxBody(physicsWorld,
@@ -249,6 +349,19 @@ public class LevelLoader extends EntityLoader<SimpleLevelEntityLoaderData> {
 		return zoneSprite;
 	}
 
+	/**
+	 * Generalized method for loading platforms.
+	 * 
+	 * @param x
+	 *            x-position for the platform
+	 * @param y
+	 *            y-position for the platform
+	 * @param platform
+	 *            the string which will be used to identify the platform
+	 * @param platformRegion
+	 *            the texture that will be used to show the platform
+	 * @return
+	 */
 	private Sprite loadPlatform(final int x, final int y, String platform,
 			ITextureRegion platformRegion) {
 		Sprite platformSprite = new Sprite(x, y, platformRegion, vbom) {
@@ -256,7 +369,13 @@ public class LevelLoader extends EntityLoader<SimpleLevelEntityLoaderData> {
 			protected void onManagedUpdate(float pSecondsElapsed) {
 				super.onManagedUpdate(pSecondsElapsed);
 				if (Upgrades.Shrooms.isActivated()) {
-					registerEntityModifier(new LoopEntityModifier(new ColorModifier(1, (float)Math.random(), (float)Math.random(), (float)Math.random(), (float)Math.random(), (float)Math.random(), (float)Math.random())));
+					registerEntityModifier(new LoopEntityModifier(
+							new ColorModifier(1, (float) Math.random(),
+									(float) Math.random(),
+									(float) Math.random(),
+									(float) Math.random(),
+									(float) Math.random(),
+									(float) Math.random())));
 				}
 			}
 		};
